ef3ef4995a00e017355c10a8639571e4761da9e8
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-01 20:52:43 -0300

    feat: add initial structure to NewTransactionModal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,53 @@
-import { Dashboard } from './components/Dashboard';
-import { Header } from './components/Header';
-import { createServer } from 'miragejs';
-import { GlobalStyle } from './styles/global';
+import { Dashboard } from "./components/Dashboard";
+import Modal from "react-modal";
+import { Header } from "./components/Header";
+import { createServer } from "miragejs";
+import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 createServer({
   routes() {
-    this.namespace = 'api';
+    this.namespace = "api";
 
-    this.get('/transactions', () => {
+    this.get("/transactions", () => {
       return [
         {
           id: 1,
-          title: 'Transaction 1',
+          title: "Transaction 1",
           amount: 400,
-          type: 'deposit',
-          category: 'Food',
+          type: "deposit",
+          category: "Food",
           createAt: new Date(),
-        }
+        },
       ];
-    })
-  }
+    });
+  },
 });
 
+Modal.setAppElement("#root");
+
 function App() {
+  const [isTransactionModalOpen, setIsTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -20,12 +20,6 @@ export function TransactionsTable() {
         <tbody>
           <tr>
             <td>Desenvolvimento de website</td>
-            <td>R$ 12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento de website</td>
             <td className="deposit">R$ 12.000</td>
             <td>Desenvolvimento</td>
             <td>20/02/2021</td>
