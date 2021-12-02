d3dee7b9a41b91f0f10911341cd611a2a02ce3bb
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-01 22:48:03 -0300

    feat(TransactionsTable): fetch transactions and add dynamic content

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 import * as S from './styles';
 
+interface Transaction {
+  id: number;
+  title: string;
+  type: 'deposity' | 'withdraw';
+  category: string;
+  amount: number;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([] as Transaction[]);
+
   useEffect(() => {
     api.get('/transactions')
-      .then(response => console.log(response.data));
+      .then(response => setTransactions(response.data.transactions));
   }, []);
   
   return (
@@ -18,18 +29,14 @@ export function TransactionsTable() {
           <th>Data</th>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$ 12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="withdraw">-R$ 12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </S.Container>
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,37 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Conta de energia',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 160,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 600,
+          createdAt: new Date('2021-02-09 09:00:00'),
+        },
+        {
+          id: 3,
+          title: 'Salário',
+          type: 'deposit',
+          category: 'Trabalho',
+          amount: 1500,
+          createdAt: new Date('2021-02-05 09:00:00'),
+        }
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
