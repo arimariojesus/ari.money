b3d85a3b149f7d4ac33bc757709181d6fb581046
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-01 22:31:55 -0300

    feat: add form values and create models from miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
 import { Dashboard } from "./components/Dashboard";
 import Modal from "react-modal";
 import { Header } from "./components/Header";
-import { createServer } from "miragejs";
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-
-createServer({
-  routes() {
-    this.namespace = "api";
-
-    this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          type: "deposit",
-          category: "Food",
-          createAt: new Date(),
-        },
-      ];
-    });
-  },
-});
 
 Modal.setAppElement("#root");
 
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,23 +1,42 @@
+import { useState, FormEvent } from "react";
+
 import Modal from "react-modal";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
 
+import { api } from "../../services/api";
+
 import * as S from "./styles";
-import { useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
-type Type = "deposity" | "withdrawn";
+type TransactionType = "deposity" | "withdrawn";
 
 export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-  const [type, setType] = useState<Type>("deposity");
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+  const [type, setType] = useState<TransactionType>("deposity");
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type,
+    };
+
+    api.post('/transactions', data);
+  }
 
   return (
     <Modal
@@ -34,12 +53,22 @@ export function NewTransactionModal({
         <img src={closeImg} alt="Fechar modal" />
       </button>
 
-      <S.Container>
+      <S.Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
-        <input type="text" placeholder="Título" />
+        <input
+          type="text"
+          placeholder="Título"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
 
-        <input type="number" placeholder="Valor" />
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={e => setValue(Number(e.target.value))}
+        />
 
         <S.TransactionTypeContainer>
           <S.RadioBox
@@ -63,7 +92,12 @@ export function NewTransactionModal({
           </S.RadioBox>
         </S.TransactionTypeContainer>
 
-        <input type="text" placeholder="Categoria" />
+        <input
+          type="text"
+          placeholder="Categoria"
+          value={category}
+          onChange={e => setCategory(e.target.value)}
+        />
 
         <button type="submit">Cadastrar</button>
       </S.Container>
