022e163f2c5ebc171c15ed22f0fe32cc580a50bf
arimariojesus <arimario.jesus@hotmail.com>
2022-08-16 01:39:38 -0300

    test: ensure image on header have correct src

--- src/components/Header/header.spec.tsx
+++ src/components/Header/header.spec.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { Header } from ".";
 
+import logoImg from '../../assets/logo.svg';
+
 const makeSut = (onOpenNewTransactionModal = () => {}): void => {
   render(<Header onOpenNewTransactionModal={onOpenNewTransactionModal} />);
 };
@@ -9,5 +11,10 @@ describe("Header", () => {
   it("should render", () => {
     makeSut();
     expect(screen.getByRole('banner')).toBeInTheDocument();
+  });
+  
+  it("should img have correct src", () => {
+    makeSut();
+    expect(screen.getByAltText('ari money')).toHaveAttribute('src', logoImg);
   });
 });
