0375b4212d60062f2110d517089b176548256b37
arimariojesus <arimario.jesus@hotmail.com>
2022-08-16 01:48:53 -0300

    test: ensure function is called when click on new transaction button

--- src/components/Header/header.spec.tsx
+++ src/components/Header/header.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Header } from ".";
 
 import logoImg from '../../assets/logo.svg';
@@ -16,5 +17,17 @@ describe("Header", () => {
   it("should img have correct src", () => {
     makeSut();
     expect(screen.getByAltText('ari money')).toHaveAttribute('src', logoImg);
+  });
+  
+  it("should call function on click new transaction button", () => {
+    const func = jest.fn();
+
+    makeSut(func);
+
+    const button = screen.getByRole('button', { name: /nova transação/i })
+
+    userEvent.click(button)
+    
+    expect(func).toHaveBeenCalledTimes(1);
   });
 });
