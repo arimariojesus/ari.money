c6314e2e9beb4e6d5f2f74dbc96004e5e3df7759
arimariojesus <arimario.jesus@hotmail.com>
2022-08-16 21:18:24 -0300

    test: ensure render images with correct src

--- src/components/Summary/summary.spec.tsx
+++ src/components/Summary/summary.spec.tsx
@@ -1,6 +1,10 @@
 import { render, screen } from "../../utils/test-utils";
 import { Summary } from ".";
 
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
 const makeSut = (): void => {
   render(<Summary />);
 };
@@ -11,5 +15,12 @@ describe("Header", () => {
     expect(screen.getByText(/entradas/i)).toBeInTheDocument();
     expect(screen.getByText(/saídas/i)).toBeInTheDocument();
     expect(screen.getByText(/total/i)).toBeInTheDocument();
+  });
+
+  it("should render images with correct src", () => {
+    makeSut();
+    expect(screen.getByRole('img', { name: /entradas/i })).toHaveAttribute('src', incomeImg);
+    expect(screen.getByRole('img', { name: /saídas/i })).toHaveAttribute('src', outcomeImg);
+    expect(screen.getByRole('img', { name: /total/i })).toHaveAttribute('src', totalImg);
   });
 });
