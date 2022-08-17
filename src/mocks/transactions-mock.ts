c91bc5b9e65bdd36ac8263adf0a1fe24b83b7e7d
arimariojesus <arimario.jesus@hotmail.com>
2022-08-16 22:55:35 -0300

    refactor: add type to transactions mock

--- src/mocks/transactions-mock.ts
+++ src/mocks/transactions-mock.ts
@@ -1,26 +1,28 @@
-export const transactionsMock = [
+import { Transaction } from "../types/transaction";
+
+export const transactionsMock: Transaction[] = [
   {
     id: 1,
-    title: 'Conta de energia',
-    type: 'withdraw',
-    category: 'Casa',
+    title: "Conta de energia",
+    type: "withdraw",
+    category: "Casa",
     amount: 160,
-    createdAt: new Date('2021-02-12 09:00:00'),
+    createdAt: new Date("2021-02-12 09:00:00"),
   },
   {
     id: 2,
-    title: 'Aluguel',
-    type: 'withdraw',
-    category: 'Casa',
+    title: "Aluguel",
+    type: "withdraw",
+    category: "Casa",
     amount: 600,
-    createdAt: new Date('2021-02-09 09:00:00'),
+    createdAt: new Date("2021-02-09 09:00:00"),
   },
   {
     id: 3,
-    title: 'Salário',
-    type: 'deposit',
-    category: 'Trabalho',
+    title: "Salário",
+    type: "deposit",
+    category: "Trabalho",
     amount: 1500,
-    createdAt: new Date('2021-02-05 09:00:00'),
-  }
+    createdAt: new Date("2021-02-05 09:00:00"),
+  },
 ];
