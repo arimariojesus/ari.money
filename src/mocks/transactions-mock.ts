b3956dc853f35830ef45cb2be8734d4f0a45ce5f
arimariojesus <arimario.jesus@hotmail.com>
2022-08-16 21:43:50 -0300

    feat: add transactions mocks

---
+++ src/mocks/transactions-mock.ts
@@ -0,0 +1,26 @@
+export const transactionsMock = [
+  {
+    id: 1,
+    title: 'Conta de energia',
+    type: 'withdraw',
+    category: 'Casa',
+    amount: 160,
+    createdAt: new Date('2021-02-12 09:00:00'),
+  },
+  {
+    id: 2,
+    title: 'Aluguel',
+    type: 'withdraw',
+    category: 'Casa',
+    amount: 600,
+    createdAt: new Date('2021-02-09 09:00:00'),
+  },
+  {
+    id: 3,
+    title: 'Salário',
+    type: 'deposit',
+    category: 'Trabalho',
+    amount: 1500,
+    createdAt: new Date('2021-02-05 09:00:00'),
+  }
+];
