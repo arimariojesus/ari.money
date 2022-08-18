b47b0f790b6df2eb95735475c255ddd230a799b0
arimariojesus <arimario.jesus@hotmail.com>
2022-08-18 03:30:42 -0300

    test: ensure formatCurrencyValue throw an error if called with an invalid value

--- src/utils/format-currency-value/format-currency-value.spec.ts
+++ src/utils/format-currency-value/format-currency-value.spec.ts
@@ -7,4 +7,27 @@ describe('formatCurrencyValue', () => {
     expect(formatCurrencyValue(-10)).toBe('-R$\xa010,00');
     expect(formatCurrencyValue(1000)).toBe('R$\xa01.000,00');
   });
+  
+  it('should throw an error if called with an invalid value', () => {
+    const func1 = () => {
+      // @ts-ignore
+      formatCurrencyValue('test');
+    };
+    
+    expect(func1).toThrow(TypeError);
+
+    const func2 = () => {
+      // @ts-ignore
+      formatCurrencyValue(null);
+    };
+    
+    expect(func2).toThrow(TypeError);
+
+    const func3 = () => {
+      // @ts-ignore
+      formatCurrencyValue(undefined);
+    };
+    
+    expect(func3).toThrow(TypeError);
+  });
 });
