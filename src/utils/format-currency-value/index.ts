ce848cb19797efd5e875f10d76f1ac379929f5fd
arimariojesus <arimario.jesus@hotmail.com>
2022-08-18 03:30:32 -0300

    feat: ensure formatCurrencyValue throw an error if called with an invalid value

--- src/utils/format-currency-value/index.ts
+++ src/utils/format-currency-value/index.ts
@@ -1,4 +1,8 @@
 export function formatCurrencyValue(value: number) {
+  if (isNaN(value) || (!value && value !== 0)) {
+    throw new TypeError('You must provide a number to formatCurrencyValue!');
+  }
+  
   const currency = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
