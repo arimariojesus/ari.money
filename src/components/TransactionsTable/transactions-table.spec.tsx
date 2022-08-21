5dc9033373a6abea179e8f66b06c42a2028e7e4e
arimariojesus <arimario.jesus@hotmail.com>
2022-08-20 21:42:56 -0300

    test: ensure TransactionsTable map correct amount of items

--- src/components/TransactionsTable/transactions-table.spec.tsx
+++ src/components/TransactionsTable/transactions-table.spec.tsx
@@ -1,5 +1,8 @@
-import { render, screen } from "../../utils";
 import { TransactionsTable } from ".";
+import { render, screen, storageTransactions } from "../../utils";
+import { transactionsMock } from "../../mocks";
+
+const { key } = storageTransactions;
 
 describe('TransactionsTable Component', () => {
   it('should have correct header cells', () => {
@@ -15,5 +18,19 @@ describe('TransactionsTable Component', () => {
   it('should render empty initally', () => {
     render(<TransactionsTable />);
     expect(screen.queryAllByRole('cell')).toHaveLength(0);
+  });
+  
+  it('should map correct amount of items', () => {
+    window.localStorage.setItem(key, JSON.stringify(transactionsMock));
+    
+    const { unmount } = render(<TransactionsTable />);
+    
+    expect(screen.getAllByRole('row')).toHaveLength(transactionsMock.length + 1); // items + rowheader
+    
+    unmount();
+    window.localStorage.clear();
+    render(<TransactionsTable />);
+    
+    expect(screen.getAllByRole('row')).toHaveLength(1);
   });
 });
