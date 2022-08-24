18c6080403cd319471548723f6764f38b6b27be4
arimariojesus <arimario.jesus@hotmail.com>
2022-08-23 22:52:25 -0300

    test: ensure NeTransactionModal call onRequestClose when close button is clicked

--- src/components/NewTransactionModal/new-transaction-modal.spec.tsx
+++ src/components/NewTransactionModal/new-transaction-modal.spec.tsx
@@ -1,9 +1,10 @@
 import Modal from "react-modal";
-import { render, screen } from "../../utils";
+import { act, render, screen } from "../../utils";
 import { NewTransactionModal } from ".";
 import { useEffect } from "react";
+import userEvent from "@testing-library/user-event";
 
-const makeSut = (isOpen = false, onRequestClose = () => undefined) => {
+const makeSut = (isOpen = true, onRequestClose = () => undefined) => {
   const ElementHelper = () => {
     useEffect(() => {
       Modal.setAppElement('#test');
@@ -21,16 +22,30 @@ const makeSut = (isOpen = false, onRequestClose = () => undefined) => {
 
 describe("NewTransactionModal Component", () => {
   it("should not show the modal when isOpen is false", () => {
-    makeSut();
+    makeSut(false);
 
     const modal = screen.queryByRole('dialog');
     expect(modal).toBeNull();
   });
   
   it("should show modal the when isOpen is true", () => {
-    makeSut(true);
+    makeSut();
 
     const modal = screen.queryByRole('dialog');
     expect(modal).toBeInTheDocument();
+  });
+  
+  it("should call onRequestClose when close button is clicked", () => {
+    const onRequestClose = jest.fn();
+    
+    makeSut(true, onRequestClose);
+
+    const closeButton = screen.getByAltText(/fechar modal/i);
+    
+    act(() => {
+      userEvent.click(closeButton);
+    });
+
+    expect(onRequestClose).toBeCalledTimes(1);
   });
 });
