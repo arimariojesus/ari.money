56b9ca7aa078d3dd6aff761bccdbafde62d9ebae
arimariojesus <arimario.jesus@hotmail.com>
2022-08-23 22:46:51 -0300

    test: ensure NeTransactionModal not show the modal when isOpen is false

---
+++ src/components/NewTransactionModal/new-transaction-modal.spec.tsx
@@ -0,0 +1,29 @@
+import Modal from "react-modal";
+import { render, screen } from "../../utils";
+import { NewTransactionModal } from ".";
+import { useEffect } from "react";
+
+const makeSut = (isOpen = false, onRequestClose = () => undefined) => {
+  const ElementHelper = () => {
+    useEffect(() => {
+      Modal.setAppElement('#test');
+    });
+    
+    return (
+      <div id="test">
+        <NewTransactionModal isOpen={isOpen} onRequestClose={onRequestClose} />
+      </div>
+    );
+  };
+  
+  render(<ElementHelper />);
+};
+
+describe("NewTransactionModal Component", () => {
+  it("should not show the modal when isOpen is false", () => {
+    makeSut();
+
+    const modal = screen.queryByRole('dialog');
+    expect(modal).toBeNull();
+  });
+});
